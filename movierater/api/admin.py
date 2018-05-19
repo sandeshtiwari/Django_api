e57b5c534c9c1566937b0f20eb384f133d25b2c3
Sandesh Tiwari <sandeshtiwari@live.com>
2018-05-19 18:15:54 -0500

    feat: Add feature to so that Admin can add/delete movies

--- movierater/api/admin.py
+++ movierater/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from movierater.api.models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = ('title', 'description')
+    list_display = ['title', 'description']
+    search_fields = ('title', 'description') 
