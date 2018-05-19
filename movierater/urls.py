a9ae68abe8f9c4887f8192ca2280b45a4bee762e
Sandesh Tiwari <sandeshtiwari@live.com>
2018-05-19 11:46:09 -0500

    feat: Add admin section

--- movierater/urls.py
+++ movierater/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-
+from django.contrib import admin
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('movierater.api.urls')),
 ]
