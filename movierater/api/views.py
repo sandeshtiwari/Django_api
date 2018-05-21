a66a7cad2a3cf50832e88d32f6a25002da45a26b
Sandesh Tiwari <sandeshtiwari@live.com>
2018-05-21 17:08:48 -0500

    feat: Add custom endpoint to edit and create a new rating for an existing movie

--- movierater/api/views.py
+++ movierater/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from movierater.api.serializers import UserSerializer, MovieSerializer, RatingSerializer
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -7,6 +7,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from movierater.api.models import Movie, Rating
+from rest_framework.decorators import list_route
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -30,6 +31,28 @@ class RatingViewSet(viewsets.ModelViewSet):
     serializer_class = RatingSerializer
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
+
+    @list_route(methods=['post'])
+    def rate_movie(self, request):
+        if 'movie' in request.data and 'user' in request.data and 'stars' in request.data:
+            movie = Movie.objects.get(id=request.data['movie'])
+            user = User.objects.get(id=request.data['user'])
+            stars = request.data['stars']
+            try:
+                my_rating = Rating.objects.get(movie=movie.id, user=user.id)
+                my_rating.stars = stars
+                my_rating.save()
+                serializer = MovieSerializer(movie, many=False)
+                response = {'message':'Rating updated successfully!', 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                Rating.objects.create(movie=movie, user=user, stars=stars)
+                serializer = MovieSerializer(movie, many=False)
+                response = {'message':'Rating created successfully!', 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'You need to pass all required parameters'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class CustomObtainAuthToken(ObtainAuthToken):
 
